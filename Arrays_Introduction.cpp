cdaef7829ef3fb865acec43caf60113b82fafba4
Saksham Aurora <49725699+sakshm-aurora@users.noreply.github.com>
2020-04-17 06:10:28 +0530

    Create Arrays_Introduction.cpp

---
+++ Arrays_Introduction.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    int i;
+    scanf("%d",&n);
+    int arr[n] ;
+    for(i=0;i<=n-1;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
